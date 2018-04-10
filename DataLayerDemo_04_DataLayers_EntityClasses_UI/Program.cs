54aa75ba3360e327b86b2fd4fc26d68d04085914
lclement <lclement@DESKTOP-B14LF2G>
2018-04-09 17:20:12 -0700

    Add project files.

---
+++ DataLayerDemo_04_DataLayers_EntityClasses_UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerDemo_04_DataLayers_EntityClasses_UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
