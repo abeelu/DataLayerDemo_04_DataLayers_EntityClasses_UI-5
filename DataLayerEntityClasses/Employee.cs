530517f8c5cb32593ba368fab0408f0e22895358
lclement <lclement@DESKTOP-B14LF2G>
2018-04-16 20:50:59 -0700

    Created a copy of the DataLayerEntityClasses within the local solution

---
+++ DataLayerEntityClasses/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace DataLayerEntityClasses
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            EmployeeID = -1;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Title = string.Empty;
+        }
+
+        public int EmployeeID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Title { get; set; }
+
+        public override string ToString()
+        {
+            return this.EmployeeID.ToString() + "/" + this.LastName + ", " + this.FirstName + "/" + this.Title;
+        }
+    }
+}
